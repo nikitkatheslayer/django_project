068ddbe1859820ccce9d70e3daed28efda382b57
nikitkatheslayer <nikita_fadeev_01@inbox.ru>
2023-06-27 15:12:13 +0300

    привязал страницы к ссылкам

--- stayersite/stayersite/urls.py
+++ stayersite/stayersite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from mainapp import views
 
 urlpatterns = [
+    path('', views.index),
+    path('contact/', views.contact),
+    path('products/', views.products),
     path('admin/', admin.site.urls),
 ]
